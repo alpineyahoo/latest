8695ca6b80f75c8b38820bec214f37d633bd3f0c
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 19:19:04 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
 mkdir -p $HOME/Library/Application\ Support/alpineyahoo.latest
 curl -sL https://github.com/alpineyahoo/latest/raw/main/repos.txt >> $HOME/Library/Application\ Support/alpineyahoo.latest/repos.txt
 curl -sL https://github.com/alpineyahoo/latest/raw/main/latest.sh >> latest
