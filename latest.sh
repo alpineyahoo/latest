3de1a5ca108d21e1b8cfc7646bafa6a3336ab021
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 22:05:46 +0900

    update latest.sh

--- latest.sh
+++ latest.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env zsh
 
-export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
-echo "Select repo(s):"
-repos=$(gum choose --no-limit < $HOME/Library/Application\ Support/alpineyahoo.latest/repos.txt)
-latests=$(for i in $repos; do curl -sL https://api.github.com/repos/${i}/releases/latest |
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin" &&
+repos="$HOME/Library/Application Support/alpineyahoo.latest/repos.txt"
+echo "Select repo(s):" &&
+latests=$(for i in $(gum choose --no-limit < $repos)
+do
+curl -sL https://api.github.com/repos/$i/releases/latest |
 grep browser_download_url |
 grep -e dmg -e pkg -e darwin -e osx -e mac |
-cut -d '"' -f 4; done)
-echo "Select file(s) to download:"
-selection=$(gum choose --no-limit $latests)
-for i in $selection; do wget -nv -P $HOME/Downloads $i; done
+grep -v arm |
+cut -d '"' -f 4
+done) &&
+echo "Select file(s):" &&
+for i in $(echo $latests | gum choose --no-limit)
+do
+wget -q --show-progress -P $HOME/Downloads $i &&
+name=$(echo $i | rev | cut -d '/' -f 1 | rev) &&
+osascript -e '
+	on run argv
+		display notification ((item 1 of argv) & " downloaded") with title "latest"
+	end run
+' "$name"
+done
