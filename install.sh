2e92edb9bffe8d57e6b79b86873dfb1801a9917a
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 22:18:37 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
 mkdir -p $HOME/Library/Application\ Support/alpineyahoo.latest
 curl -sL https://github.com/alpineyahoo/latest/raw/main/repos.txt >> $HOME/Library/Application\ Support/alpineyahoo.latest/repos.txt
-curl -sL https://github.com/alpineyahoo/latest/raw/main/latest.sh >> latest
+echo "Which is your architecture?" &&
+curl -sL https://github.com/alpineyahoo/latest/raw/main/latest.$(gum choose arm intel).sh >> latest
 chmod 700 latest
 echo "Executable \"latest\" is downloaded in current directory.\nMove it to desired location, e.g. mv latest /usr/local/bin/latest"
