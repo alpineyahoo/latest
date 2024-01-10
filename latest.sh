3320e49a832f065163853876f5848d87f91242ff
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 18:51:35 +0900

    edit latest.sh

---
+++ latest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
+echo "Select repo(s):"
+repos=$(gum choose --no-limit < $HOME/Library/Application\ Support/alpineyahoo.latest/repos.txt)
+latests=$(for i in $repos; do curl -sL https://api.github.com/repos/${i}/releases/latest |
+grep browser_download_url |
+grep -e dmg -e pkg -e darwin -e osx -e mac |
+cut -d '"' -f 4; done)
+echo "Select file(s) to download:"
+selection=$(gum choose --no-limit $latests)
+for i in $selection; do wget -nv -P $HOME/Downloads $i; done
