4785823b27f6b6cd6f361cbc8cfcc5a6fd443885
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-01-10 19:24:12 +0900

    edit install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ mkdir -p $HOME/Library/Application\ Support/alpineyahoo.latest
 curl -sL https://github.com/alpineyahoo/latest/raw/main/repos.txt >> $HOME/Library/Application\ Support/alpineyahoo.latest/repos.txt
 curl -sL https://github.com/alpineyahoo/latest/raw/main/latest.sh >> latest
 chmod 700 latest
-echo "Executable \"latest\" is downloaded in current directory.\nMove it to desired location, e.g. /usr/local/bin/latest"
+echo "Executable \"latest\" is downloaded in current directory.\nMove it to desired location, e.g. mv latest /usr/local/bin/latest"
