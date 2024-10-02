57f1289154d367465b017abfe24c5b3a6197d55f
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-02 10:54:25 +0900

    create and edit test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# echo "apple/pkl" |
+# xargs -I _ gh release list -R _ --exclude-drafts --exclude-pre-releases \
+# --json createdAt,isDraft,isLatest,isPrerelease,name,publishedAt,tagName -q '.[] | select(.isLatest == true)' |
+# jq
+
+# 最新版リリースのタグ値取得. "-L 3" オプションは1でもいい気がするが念のため3
+gh release list -R apple/pkl --exclude-drafts --exclude-pre-releases -L 3 |
+grep Latest |
+awk '{print $3}' |
+# xargs -I _ gh release view _ -R apple/pkl
+xargs -I _ gh release view _ -R apple/pkl \
+--json apiUrl,assets,author,body,createdAt,databaseId,id,name,publishedAt,tagName,tarballUrl,targetCommitish,uploadUrl,url,zipballUrl \
+-q '.assets[] | select(.name | test("mac|osx|darwin"))' |
+jq
