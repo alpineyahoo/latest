3bbe718f12ff1c3eced57731304d5addedb02a1d
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-05 18:17:59 +0900

    edit latest.intel.sh

--- latest.intel.sh
+++ latest.intel.sh
@@ -22,6 +22,18 @@ EOF
 
 done
 
+for i (~/Downloads/*.pkg(N)) (
+  sudo installer -pkg "$i" -target / &&
+  rm "$i"
+)
+for i (~/Downloads/*.dmg(N)) (
+  volume=$(hdiutil attach "$i" | awk 'END{print $3}')
+  for j ($volume/*.app(N)) sudo command cp -R "$j" /Applications # overwrites existing app
+  for j ($volume/*.pkg(N)) sudo installer -pkg "$j" -target /
+  hdiutil detach "$volume" &&
+  rm "$i"
+)
+
 # command ls -1 ~/Downloads | grep -E 'dmg|pkg' | fzf --reverse | xargs -I _ echo ~/Downloads/_
 # for i in ~/Downloads/*.pkg(N); do hello; done
 # for i in ~/Downloads/*.dmg(N); do hello; done
