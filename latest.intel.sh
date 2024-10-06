b0cc49206f289dd3de8fb23835866276ce8e8ae3
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-06 11:22:38 +0900

    edit latest.intel.sh

--- latest.intel.sh
+++ latest.intel.sh
@@ -23,13 +23,15 @@ EOF
 done
 
 for i (~/Downloads/*.pkg(N)) (
+  echo "Installing $i"
   sudo installer -pkg "$i" -target / &&
   rm "$i"
 )
 for i (~/Downloads/*.dmg(N)) (
   volume=$(hdiutil attach "$i" | awk 'END{print $3}')
-  for j ($volume/*.app(N)) sudo command cp -R "$j" /Applications # overwrites existing app
-  for j ($volume/*.pkg(N)) sudo installer -pkg "$j" -target /
+  echo "Installing $i"
+  for j ("$volume"/*.app(N)) sudo command cp -R "$j" /Applications # overwrites existing app
+  for j ("$volume"/*.pkg(N)) sudo installer -pkg "$j" -target /
   hdiutil detach "$volume" &&
   rm "$i"
 )
