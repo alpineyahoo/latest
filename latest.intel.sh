334d246a295a728495719d7d4f6c15604dc9dee5
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-11 14:40:03 +0900

    Revert "edit latest.intel.sh"
    This reverts commit feca5d78b04a40bab042a92e50daa0b88b666942.

--- latest.intel.sh
+++ latest.intel.sh
@@ -3,13 +3,14 @@
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin" &&
 repos="$HOME/Library/Application Support/alpineyahoo.latest/repos.txt"
 echo "Select repo(s):" &&
-for i in $(gum choose --no-limit < $repos); do
-  curl -sL https://api.github.com/repos/$i/releases/latest |
-  grep browser_download_url |
-  grep -E 'dmg|pkg|darwin|osx|mac' |
-  grep -v arm |
-  cut -d '"' -f 4
-done | read -d '' latests &&
+latests=$(for i in $(gum choose --no-limit < $repos)
+do
+curl -sL https://api.github.com/repos/$i/releases/latest |
+grep browser_download_url |
+grep -E 'dmg|pkg|darwin|osx|mac' |
+grep -v arm |
+cut -d '"' -f 4
+done) &&
 echo "Select file(s):" &&
 for i in $(echo $latests | gum choose --no-limit)
 do
