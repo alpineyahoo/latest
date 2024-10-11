feca5d78b04a40bab042a92e50daa0b88b666942
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-10-11 14:25:21 +0900

    edit latest.intel.sh

--- latest.intel.sh
+++ latest.intel.sh
@@ -3,14 +3,13 @@
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin" &&
 repos="$HOME/Library/Application Support/alpineyahoo.latest/repos.txt"
 echo "Select repo(s):" &&
-latests=$(for i in $(gum choose --no-limit < $repos)
-do
-curl -sL https://api.github.com/repos/$i/releases/latest |
-grep browser_download_url |
-grep -E 'dmg|pkg|darwin|osx|mac' |
-grep -v arm |
-cut -d '"' -f 4
-done) &&
+for i in $(gum choose --no-limit < $repos); do
+  curl -sL https://api.github.com/repos/$i/releases/latest |
+  grep browser_download_url |
+  grep -E 'dmg|pkg|darwin|osx|mac' |
+  grep -v arm |
+  cut -d '"' -f 4
+done | read -d '' latests &&
 echo "Select file(s):" &&
 for i in $(echo $latests | gum choose --no-limit)
 do
