02a86f5e81748710d338f118c68954a906a392bf
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2025-09-16 06:53:56 +0900

    update latest.sh

--- latest.sh
+++ latest.sh
@@ -36,13 +36,13 @@ EOF
 done
 
 for i (~/Downloads/*.pkg(N)) (
-  echo "Installing $i"
+  echo "Installing $(basename $i)"
   sudo installer -pkg "$i" -target / &&
   rm "$i"
 )
 for i (~/Downloads/*.dmg(N)) (
   volume=$(hdiutil attach "$i" | tail -n 1 | cut -f 3-) # 契約書を認める必要あり(Agree Y/N?)
-  echo "Installing $i"
+  echo "Installing $(basename $i)"
   for j ("$volume"/*.app(N)) sudo command cp -R "$j" /Applications # overwrites existing app
   for j ("$volume"/*.pkg(N)) sudo installer -pkg "$j" -target /
   hdiutil detach "$volume" &&
